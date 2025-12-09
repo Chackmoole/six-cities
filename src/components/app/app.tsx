82178ae5a632e88d90624c230977b17929b4e235
chackmol <cebep-21@mail.ru>
2025-12-09 08:40:39 +0300

    Добавлен простой роутинг.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,28 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Main} from '../../pages/main/main';
+import {Favorites} from '../../pages/favorites/favorites';
+import {FavoritesEmpty} from '../../pages/favorites-empty/favorites-empty';
+import {Login} from '../../pages/login/login';
+import {MainEmpty} from '../../pages/main-empty/main-empty';
+import {Property} from '../../pages/property/property';
+import {PropertyNotLogged} from '../../pages/property-not-logged/property-not-logged';
 
 interface IProps {
   offersCount: number;
 }
 
 export const App = ({offersCount}:IProps) => (
-  <Main offersCount={offersCount}/>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Main offersCount={offersCount}/>} />
+      <Route path={'/favorites'} element={<Favorites/>}/>
+      <Route path={'favorites-empty'} element={<FavoritesEmpty/>}/>
+      <Route path={'login'} element={<Login/>}/>
+      <Route path={'main-empty'} element={<MainEmpty/>}/>
+      <Route path={'property'} element={<Property/>}/>
+      <Route path={'property-not-logged'} element={<PropertyNotLogged/>}/>
+    </Routes>
+  </BrowserRouter>
 );
 
 
