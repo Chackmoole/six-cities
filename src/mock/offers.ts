518cbe31ed809545983a074e6adb8f24523832f7
chackmol <cebep-21@mail.ru>
2025-12-11 15:31:42 +0300

    Добавлены моковые данные.

---
+++ src/mock/offers.ts
@@ -0,0 +1,229 @@
+export const HOTELS = [
+
+  {
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'previewImage': 'https://10.react.htmlacademy.pro/static/hotel/14.jpg',
+    'images': [
+      'https://10.react.htmlacademy.pro/static/hotel/13.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/8.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/15.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/20.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/3.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/1.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/11.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/16.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/7.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/4.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/9.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/14.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/18.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/10.jpg'
+    ],
+    'title': 'Beautiful & luxurious apartment at great location',
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 3.9,
+    'type': 'house',
+    'bedrooms': 2,
+    'maxAdults': 6,
+    'price': 766,
+    'goods': [
+      'Laptop friendly workspace',
+      'Air conditioning',
+      'Baby seat',
+      'Washer',
+      'Towels',
+      'Breakfast'
+    ],
+    'host': {
+      'id': 25,
+      'name': 'Angelina',
+      'isPro': true,
+      'avatarUrl': 'img/avatar-angelina.jpg'
+    },
+    'description': 'Discover daily local life in city center, friendly neighborhood, clandestine casino, karaoke, old-style artisans, art gallery and artist studio downstairs.',
+    'location': {
+      'latitude': 48.83961,
+      'longitude': 2.342499,
+      'zoom': 16
+    },
+    'id': 1
+  },
+
+  {
+    'city': {
+      'name': 'Dusseldorf',
+      'location': {
+        'latitude': 51.225402,
+        'longitude': 6.776314,
+        'zoom': 13
+      }
+    },
+    'previewImage': 'https://10.react.htmlacademy.pro/static/hotel/13.jpg',
+    'images': [
+      'https://10.react.htmlacademy.pro/static/hotel/8.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/12.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/10.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/9.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/19.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/20.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/4.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/11.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/1.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/17.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/2.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/15.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/5.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/7.jpg'
+    ],
+    'title': 'Penthouse, 4-5 rooms + 5 balconies',
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 2.2,
+    'type': 'house',
+    'bedrooms': 2,
+    'maxAdults': 4,
+    'price': 371,
+    'goods': [
+      'Laptop friendly workspace',
+      'Baby seat',
+      'Air conditioning',
+      'Breakfast',
+      'Fridge',
+      'Washer',
+      'Towels'
+    ],
+    'host': {
+      'id': 25,
+      'name': 'Angelina',
+      'isPro': true,
+      'avatarUrl': 'img/avatar-angelina.jpg'
+    },
+    'description': 'I am happy to welcome you to my apartment in the city center! Three words: location, cosy and chic!',
+    'location': {
+      'latitude': 51.211402,
+      'longitude': 6.756314000000001,
+      'zoom': 16
+    },
+    'id': 2
+  },
+
+  {
+    'city': {
+      'name': 'Dusseldorf',
+      'location': {
+        'latitude': 51.225402,
+        'longitude': 6.776314,
+        'zoom': 13
+      }
+    },
+    'previewImage': 'https://10.react.htmlacademy.pro/static/hotel/6.jpg',
+    'images': [
+      'https://10.react.htmlacademy.pro/static/hotel/2.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/6.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/13.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/3.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/1.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/15.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/9.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/10.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/17.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/5.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/11.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/12.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/4.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/19.jpg'
+    ],
+    'title': 'Loft Studio in the Central Area',
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 4.8,
+    'type': 'apartment',
+    'bedrooms': 4,
+    'maxAdults': 9,
+    'price': 156,
+    'goods': [
+      'Towels',
+      'Air conditioning',
+      'Breakfast',
+      'Washer',
+      'Fridge',
+      'Baby seat',
+      'Laptop friendly workspace'
+    ],
+    'host': {
+      'id': 25,
+      'name': 'Angelina',
+      'isPro': true,
+      'avatarUrl': 'img/avatar-angelina.jpg'
+    },
+    'description': 'This is a place for dreamers to reset, reflect, and create. Designed with a \'slow\' pace in mind, our hope is that you enjoy every part of your stay; from making local coffee by drip in the morning, choosing the perfect record to put on as the sun sets.',
+    'location': {
+      'latitude': 51.241402,
+      'longitude': 6.782314,
+      'zoom': 16
+    },
+    'id': 3
+  },
+
+  {
+    'city': {
+      'name': 'Amsterdam',
+      'location': {
+        'latitude': 52.37454,
+        'longitude': 4.897976,
+        'zoom': 13
+      }
+    },
+    'previewImage': 'https://10.react.htmlacademy.pro/static/hotel/18.jpg',
+    'images': [
+      'https://10.react.htmlacademy.pro/static/hotel/11.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/8.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/14.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/1.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/17.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/7.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/15.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/18.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/19.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/20.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/13.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/5.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/4.jpg',
+      'https://10.react.htmlacademy.pro/static/hotel/9.jpg'
+    ],
+    'title': 'House in countryside',
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 3.1,
+    'type': 'apartment',
+    'bedrooms': 3,
+    'maxAdults': 7,
+    'price': 243,
+    'goods': [
+      'Washer',
+      'Breakfast',
+      'Laptop friendly workspace'
+    ],
+    'host': {
+      'id': 25,
+      'name': 'Angelina',
+      'isPro': true,
+      'avatarUrl': 'img/avatar-angelina.jpg'
+    },
+    'description': 'Relax, rejuvenate and unplug in this ultimate rustic getaway experience in the country. In our beautiful screened Pondhouse, you can gaze at the stars and listen to the sounds of nature from your cozy warm bed.',
+    'location': {
+      'latitude': 52.37854,
+      'longitude': 4.894976,
+      'zoom': 16
+    },
+    'id': 4
+  }
+];
