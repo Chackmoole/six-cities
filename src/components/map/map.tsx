8c338a34e60b7b7f833d7f1eee284b2817a0fe03
chackmol <cebep-21@mail.ru>
2026-01-23 17:50:05 +0300

    Добавлен кастомный пин на карту

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,6 +1,7 @@
 import {useRef, useEffect} from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import iconMarker from '../../assets/img/pin.svg';
 
 interface Location {
     latitude: number;
@@ -20,12 +21,22 @@ export const Map = ({city}: IProps) => {
 
   useEffect(() => {
     if (mapContainer.current) {
-      new maplibregl.Map({
+      const map = new maplibregl.Map({
         container: mapContainer.current,
         style: 'https://tiles.openfreemap.org/styles/bright',
         center: [city.location.longitude, city.location.latitude],
         zoom: city.location.zoom,
       });
+
+      const marker = document.createElement('div');
+      marker.className = 'custom-marker';
+      marker.style.backgroundImage = `url(${iconMarker})`;
+      marker.style.width = '27px';
+      marker.style.height = '39px';
+      marker.style.cursor = 'pointer';
+      marker.style.backgroundSize = '100%';
+
+      new maplibregl.Marker({element: marker}).setLngLat([city.location.longitude, city.location.latitude]).addTo(map);
 
     }
   }, [city]);
