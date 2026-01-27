5867ec9895b73d02dbb68436ff9ba2f58c4d0415
chackmol <cebep-21@mail.ru>
2026-01-27 19:35:11 +0300

    Добавлены моковые данные для комментариев прокинуты через props. Доработка даты сообщения

--- src/components/reviews-item/reviews-item.tsx
+++ src/components/reviews-item/reviews-item.tsx
@@ -1,4 +1,5 @@
 import {IReview} from '../../types/types';
+import {conversionDate} from '../../util/util';
 
 interface IProps {
   reviews: IReview[];
@@ -9,22 +10,25 @@ export const ReviewsItem = ({reviews}:IProps) => (
   <li className="reviews__item">
     <div className="reviews__user user">
       <div className="reviews__avatar-wrapper user__avatar-wrapper">
-        <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar"/>
+        <img className="reviews__avatar user__avatar" src={reviews[0].user.avatarUrl} width="54" height="54" alt={reviews[0].user.name}/>
       </div>
       <span className="reviews__user-name">
-                        Max
+        {reviews[0].user.name}
       </span>
+      {reviews[0].user.isPro ? <span style={{fontSize: '12px', lineHeight: '1.167', color: '#696969'}}>Pro</span> : null}
     </div>
     <div className="reviews__info">
       <div className="reviews__rating rating">
         <div className="reviews__stars rating__stars">
-          <span style={{width: '80%'}}></span>
+          <span style={{width: `${reviews[0].rating * 20}%`}}></span>
           <span className="visually-hidden">Rating</span>
         </div>
       </div>
       <p className="reviews__text">
-        A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
+        {reviews[0].comment}
       </p>
-      <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
+      <time className="reviews__time" dateTime={reviews[0].date}>{conversionDate(reviews[0].date)}</time>
     </div>
-  </li>);
+  </li>
+);
+
--- src/util/util.ts
+++ src/util/util.ts
@@ -2,3 +2,23 @@ import {OFFERS} from '../mock/offers';
 
 export const createLocation = (city: string) => OFFERS.filter((offer) => offer.city.name === city).map((offer) => offer.location);
 
+export const conversionDate = (date: string) => {
+  const year = date.substring(0, 4);
+  const month = date.substring(5, 7);
+
+  switch (month) {
+    case '01': return `January ${year}`;
+    case '02': return `February ${year}`;
+    case '03': return `March ${year}`;
+    case '04': return `April ${year}`;
+    case '05': return `May ${year}`;
+    case '06': return `June ${year}`;
+    case '07': return `July ${year}`;
+    case '08': return `August ${year}`;
+    case '09': return `September ${year}`;
+    case '10': return `October ${year}`;
+    case '11': return `November ${year}`;
+    case '12': return `December ${year}`;
+    default: return 'last month';
+  }
+};
