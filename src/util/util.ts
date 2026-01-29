d28941105446a86f09fc8931e09f78158fba728a
chackmol <cebep-21@mail.ru>
2026-01-29 16:47:39 +0300

    Доработка функции обработки даты через new Date

--- src/util/util.ts
+++ src/util/util.ts
@@ -3,24 +3,12 @@ import {OFFERS} from '../mock/offers';
 export const createLocation = (city: string) => OFFERS.filter((offer) => offer.city.name === city).map((offer) => offer.location);
 
 export const conversionDate = (date: string) => {
-  const year = date.substring(0, 4);
-  const month = date.substring(5, 7);
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const commentDate = new Date(date);
+  const year = commentDate.getFullYear();
+  const month = commentDate.getMonth();
 
-  switch (month) {
-    case '01': return `January ${year}`;
-    case '02': return `February ${year}`;
-    case '03': return `March ${year}`;
-    case '04': return `April ${year}`;
-    case '05': return `May ${year}`;
-    case '06': return `June ${year}`;
-    case '07': return `July ${year}`;
-    case '08': return `August ${year}`;
-    case '09': return `September ${year}`;
-    case '10': return `October ${year}`;
-    case '11': return `November ${year}`;
-    case '12': return `December ${year}`;
-    default: return 'last month';
-  }
+  return `${monthNames[month]} ${year}`;
 };
 
 export const setCurrentCity = (activeTab:string) => {
