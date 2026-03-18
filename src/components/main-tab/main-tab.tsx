e5e88d46991af07c4f60e8c49186e3cc9d12e042
chackmol <cebep-21@mail.ru>
2026-03-18 17:05:53 +0300

    fix работы активных табов

--- src/components/main-tab/main-tab.tsx
+++ src/components/main-tab/main-tab.tsx
@@ -3,13 +3,14 @@ import {useDispatch} from 'react-redux';
 
 interface IProps {
   city: string;
+  activeTown : string;
 }
 
-export const MainTab = ({city}:IProps) => {
+export const MainTab = ({city, activeTown}:IProps) => {
   const dispatch = useDispatch();
   return(
     <li className="locations__item">
-      <div className="locations__item-link tabs__item" onClick={() => dispatch(setActiveTown(city)) }>
+      <div className={city === activeTown ? 'locations__item-link tabs__item tabs__item--active' : 'locations__item-link tabs__item'} onClick={() => dispatch(setActiveTown(city)) }>
         <span>{city}</span>
       </div>
     </li>
--- src/components/main-tabs/main-tabs.tsx
+++ src/components/main-tabs/main-tabs.tsx
@@ -1,11 +1,16 @@
 import {MainTab} from '../main-tab/main-tab';
 import {CITIES} from '../../data/cities';
+import {useSelector} from 'react-redux';
+import {getActiveTown} from '../../store/getters';
 
-export const MainTabs = () => (
-  <ul className="locations__list tabs__list">
-    {CITIES.map((city) => (
-      <MainTab key={city} city={city}/>
-    ))}
+export const MainTabs = () => {
+  const activeTown = useSelector(getActiveTown);
+  return (
+    <ul className="locations__list tabs__list">
+      {CITIES.map((city) => (
+        <MainTab key={city} city={city} activeTown={activeTown}/>
+      ))}
 
-  </ul>
-);
+    </ul>
+  );
+};
