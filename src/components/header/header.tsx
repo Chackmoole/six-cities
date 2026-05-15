fd1bf7e37da8d3c941181cf0f54148de7456a591
chackmol <cebep-21@mail.ru>
2026-05-15 16:38:35 +0300

    Доработка Header с авторизацией

---
+++ src/components/header/header.tsx
@@ -0,0 +1,42 @@
+import {getAuthorizationStatus} from '../../store/getters';
+import {useAppSelector} from '../../store/hooks';
+import {Link} from 'react-router-dom';
+
+export const Header = () => {
+  const authorizationStatus = useAppSelector(getAuthorizationStatus);
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__left">
+            <a className="header__logo-link header__logo-link--active" href="/">
+              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+            </a>
+          </div>
+          <nav className="header__nav">
+            <ul className="header__nav-list">
+              {authorizationStatus === 'auth' && (
+                <li className="header__nav-item user">
+                  <Link
+                    className="header__nav-link header__nav-link--profile"
+                    to={'/favorites'}
+                  >
+                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                    <span className="header__user-name user__name">
+                      Oliver.conner@gmail.com
+                    </span>
+                    <span className="header__favorite-count">3</span>
+                  </Link>
+                </li>)}
+              <li className="header__nav-item">
+                <Link className="header__nav-link" to={'login'}>
+                  <span className="header__signout">{authorizationStatus === 'auth' ? 'Sign out' : 'Sign in'}</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
