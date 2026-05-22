76e5c1d7a203e57e432780f4a082b910bd9e1cea
chackmol <cebep-21@mail.ru>
2026-05-22 12:54:13 +0300

    Доработан input. Интеграция input в поле email

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -1,12 +1,26 @@
+import React from 'react';
+
 interface IProps {
-  className: string;
+  className?: string;
   type?: string;
   name: string;
-  placeholder: string;
-  required: boolean;
-
+  placeholder?: string;
+  value: string;
+  required?: boolean;
+  onInput: (value: string) => void;
 }
 
-export const Input = ({className, type, name, placeholder, required}:IProps) => (
-  <input className={className} type={type} name={name} placeholder={placeholder} required={required} />
-);
+export const Input = ({className, type, name, placeholder, value, required, onInput}:IProps) => {
+  const handleInput: React.FormEventHandler<HTMLInputElement> = (event) => {
+    const target = event.target;
+    if (target instanceof HTMLInputElement) {
+      onInput(target.value);
+    }
+  };
+  return (
+    <input
+      className={className} type={type} name={name} placeholder={placeholder} value={value} required={required}
+      onInput={handleInput}
+    />
+  );
+};
