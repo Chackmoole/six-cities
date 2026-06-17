a825a64a0d292e16399d0d2a703f5183449319d1
chackmol <cebep-21@mail.ru>
2026-06-17 15:08:15 +0300

    Добавлена отрисовка картинок offer на страницу Property

--- src/pages/property/property.tsx
+++ src/pages/property/property.tsx
@@ -102,49 +102,17 @@ export const Property = () => {
           <main className="page__main page__main--property">
             <section className="property">
               <div className="property__gallery-container container">
-                <div className="property__gallery">
-                  <div className="property__image-wrapper">
-                    <img
-                      className="property__image"
-                      src="img/room.jpg"
-                      alt="Photo-studio"
-                    />
-                  </div>
-                  <div className="property__image-wrapper">
-                    <img
-                      className="property__image"
-                      src="img/apartment-01.jpg"
-                      alt="Photo-studio"
-                    />
-                  </div>
-                  <div className="property__image-wrapper">
-                    <img
-                      className="property__image"
-                      src="img/apartment-02.jpg"
-                      alt="Photo-studio"
-                    />
-                  </div>
-                  <div className="property__image-wrapper">
-                    <img
-                      className="property__image"
-                      src="img/apartment-03.jpg"
-                      alt="Photo-studio"
-                    />
-                  </div>
-                  <div className="property__image-wrapper">
-                    <img
-                      className="property__image"
-                      src="img/studio-01.jpg"
-                      alt="Photo-studio"
-                    />
-                  </div>
-                  <div className="property__image-wrapper">
-                    <img
-                      className="property__image"
-                      src="img/apartment-01.jpg"
-                      alt="Photo-studio"
-                    />
-                  </div>
+                <div className="property__gallery" style={{overflow: 'auto'}}>
+                  {offer.images ? offer.images.map((image)=> (
+                    <div className="property__image-wrapper" key={image}>
+                      <img
+                        className="property__image"
+                        src={image}
+                        alt="Photo-studio"
+                      />
+                    </div>
+                  )
+                  ) : null}
                 </div>
               </div>
               <div className="property__container container">
