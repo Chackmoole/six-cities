bba26ea9c312a47aaabb9e8c438997180f9e7515
chackmol <cebep-21@mail.ru>
2026-06-17 15:32:35 +0300

    Добавлена отрисовка рейтинга offer на страницу Property

--- src/pages/property/property.tsx
+++ src/pages/property/property.tsx
@@ -154,15 +154,15 @@ export const Property = () => {
                         </svg>
                         <span className="visually-hidden">To bookmarks</span>
                       </button>}
-
                   </div>
+
                   <div className="property__rating rating">
                     <div className="property__stars rating__stars">
-                      <span style={{width: '80%'}}></span>
+                      <span style={{width: `${offer.rating ? Number(offer.rating) * 20 : 0}%`}}></span>
                       <span className="visually-hidden">Rating</span>
                     </div>
                     <span className="property__rating-value rating__value">
-                    4.8
+                      {offer.rating}
                     </span>
                   </div>
                   <ul className="property__features">
