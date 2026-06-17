c948eb0c6de302da2e7c0cb4df7857dbd3a30eec
chackmol <cebep-21@mail.ru>
2026-06-17 15:21:05 +0300

    Добавлена отрисовка премиум статуса и кнопки "избранное" offer на страницу Property

--- src/pages/property/property.tsx
+++ src/pages/property/property.tsx
@@ -117,27 +117,44 @@ export const Property = () => {
               </div>
               <div className="property__container container">
                 <div className="property__wrapper">
-                  <div className="property__mark">
-                    <span>Premium</span>
-                  </div>
+                  {offer.isPremium ?
+                    <div className="property__mark">
+                      <span>Premium</span>
+                    </div> : null}
+
                   <div className="property__name-wrapper">
                     <h1 className="property__name">
-                      {/*Beautiful &amp; luxurious studio at great location*/}
                       {offer.title}
                     </h1>
-                    <button
-                      className="property__bookmark-button button"
-                      type="button"
-                    >
-                      <svg
-                        className="property__bookmark-icon"
-                        width="31"
-                        height="33"
+                    {offer.isFavorite ?
+                      <button
+                        className="property__bookmark-button property__bookmark-button--active button"
+                        type="button"
                       >
-                        <use xlinkHref="#icon-bookmark"></use>
-                      </svg>
-                      <span className="visually-hidden">To bookmarks</span>
-                    </button>
+                        <svg
+                          className="property__bookmark-icon"
+                          width="31"
+                          height="33"
+                        >
+                          <use xlinkHref="#icon-bookmark"></use>
+                        </svg>
+                        <span className="visually-hidden">To bookmarks</span>
+                      </button>
+                      :
+                      <button
+                        className="property__bookmark-button button"
+                        type="button"
+                      >
+                        <svg
+                          className="property__bookmark-icon"
+                          width="31"
+                          height="33"
+                        >
+                          <use xlinkHref="#icon-bookmark"></use>
+                        </svg>
+                        <span className="visually-hidden">To bookmarks</span>
+                      </button>}
+
                   </div>
                   <div className="property__rating rating">
                     <div className="property__stars rating__stars">
